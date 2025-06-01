914276dee9e2592f5292534f36f82ccf79a44acf
evert <evert@everbuild.be>
2025-06-02 00:02:19 +0200

    fixed exports

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
 import terser from '@rollup/plugin-terser';
 import { readdirSync } from 'node:fs';
+import { basename } from 'node:path';
+
+const files = readdirSync('src', { withFileTypes: true });
+const input = Object.fromEntries(files.map(e => {
+  if (e.isDirectory()) {
+    return [e.name, `src/${e.name}/index.ts`];
+  } else {
+    return [basename(e.name, '.ts'), `src/${e.name}`];
+  }
+}));
 
 export default {
-  input: readdirSync('src', { withFileTypes: true }).map(e => `src/${e.name}${(e.isDirectory() ? '/index.ts' : '')}`),
+  input,
   plugins: [
     typescript({
       include: 'src/**/*',
--- update-exports.js
+++ update-exports.js
@@ -1,23 +1,21 @@
 import { readFile, writeFile } from 'fs/promises';
-import { basename } from 'path';
 import config from './rollup.config.js';
 
 const PACKAGE_FILE = './package.json';
 
 const pack = JSON.parse(await readFile(PACKAGE_FILE, 'utf8'));
-
-pack.exports = await buildExports();
-
+pack.exports = buildExports();
 await writeFile(PACKAGE_FILE, JSON.stringify(pack, null, 2));
 
-async function buildExports() {
+function buildExports() {
   const result = {};
-  for (const file of config.input) {
-    const name = basename(file, '.ts');
+  const entries = config.input;
+  for (const name in entries) {
+    const src = entries[name];
     result[`./${name}`] = {
       import: `./dist/${name}.js`,
-      types: `./dist/dts/${name}.d.ts`
+      types: `./dist/dts/${src.substring(4, src.length - 3)}.d.ts`,
     };
   }
-  return result
+  return result;
 }
